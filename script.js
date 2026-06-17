e6032567205a73583930898b8e3627a18a0c381f
zazaclothing <zazaclothing7@gmail.com>
2026-06-17 10:23:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,6 +11,39 @@ window.addEventListener("load", function () {
   }
 });
 
+const spiderImages = [
+"spider tee front.jpeg",
+"spider tee back.jpeg"
+];
+
+let current = 0;
+
+function nextImage(){
+
+current++;
+
+if(current >= spiderImages.length){
+current = 0;
+}
+
+document.getElementById("spiderImage").src =
+spiderImages[current];
+
+}
+
+function prevImage(){
+
+current--;
+
+if(current < 0){
+current = spiderImages.length - 1;
+}
+
+document.getElementById("spiderImage").src =
+spiderImages[current];
+
+}
+
 
 // =======================
 // CART SYSTEM
@@ -84,4 +117,68 @@ function sendWhatsAppOrder() {
   const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
 
   window.open(url, "_blank");
+}
+
+const fifaProducts = {
+  messi: {
+    front: "messi front.jpeg",
+    back: "messi back.jpeg",
+    title: "Messi GOAT Edition"
+  },
+  ronaldo: {
+    front: "ronaldo front.jpeg",
+    back: "ronaldo back.jpeg",
+    title: "Ronaldo Legend Edition"
+  },
+  neymar: {
+    front: "neymar front.jpeg",
+    back: "neymar back.jpeg",
+    title: "Neymar Skill Edition"
+  }
+};
+
+let selectedPlayer = "";
+
+function openProduct(player) {
+
+  selectedPlayer = player;
+
+  document.getElementById("modal").style.display = "block";
+
+  document.getElementById("product-title").innerText =
+    fifaProducts[player].title;
+
+  document.getElementById("product-front").src =
+    fifaProducts[player].front;
+
+  document.getElementById("product-back").src =
+    fifaProducts[player].back;
+}
+
+function closeProduct() {
+  document.getElementById("modal").style.display = "none";
+}
+
+function orderFifaShirt() {
+
+  const size =
+    document.getElementById("fifa-size").value;
+
+  const shirt =
+    fifaProducts[selectedPlayer].title;
+
+  const message =
+` FIFA 2026 Order
+
+ Shirt: ${shirt}
+ Size: ${size}
+
+I would like to place an order.`;
+
+  const whatsappNumber = "94766517421";
+
+  window.open(
+    `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`,
+    "_blank"
+  );
 }
